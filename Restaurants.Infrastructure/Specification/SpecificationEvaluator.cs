83da9b3cde468f5601e1f7cdc48f3f6de1fc6b09
hishamabdalla <hishvmabdalla@gmail.com>
2025-02-07 23:26:33 +0200

    Specification Evaluator

---
+++ Restaurants.Infrastructure/Specification/SpecificationEvaluator.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Restaurants.Domain.Entities;
+using Restaurants.Domain.Specification;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurants.Infrastructure.Specification
+{
+    public class SpecificationEvaluator<TEntity,TKey> where TEntity : BaseEntity<TKey>
+    {
+        public static IQueryable<TEntity> GetQuery(IQueryable<TEntity> inputQuery, ISpecification<TEntity, TKey> specification)
+        {
+            var query = inputQuery;
+
+            if(specification.Criteria != null)
+            {
+                query=query.Where(specification.Criteria);
+            }
+
+            query = specification.Includes.Aggregate(query, (current, include) =>current.Include(include));
+
+            return query;
+        }
+
+         
+    }
+}
